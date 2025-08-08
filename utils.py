c39e9d6d27586bb6faef2e5d176e7bdb5264e023
yuvrajpant56 <yuvrajpant5@gmail.com>
2025-08-08 12:23:03 -0500

    changing utils

--- utils.py
+++ utils.py
@@ -1,7 +1,11 @@
 # utils.py
 
 def convert_to_seconds(time_str):
-    if isinstance(time_str, str):
-        h, m, s = map(int, time_str.split(':'))
-        return h * 3600 + m * 60 + s
+    try:
+        if isinstance(time_str, str):
+            h, m, s = map(int, time_str.split(':'))
+            return h * 3600 + m * 60 + s
+    except ValueError as e:
+        print(f"Invalid time format: {e}")
     return None
+
